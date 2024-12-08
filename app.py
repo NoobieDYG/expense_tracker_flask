9704286b6adb038e7d44ddd8d8b556d78fc03b69
Affaan Jaweed <affaan190@gmail.com>
2024-12-08 21:49:06 +0530

    changed folder paths

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from models import db
 from routes import routes
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__,static_folder=r'C:\\Users\\Affaan Jaweed\\Desktop\\expenset\\static',template_folder=r"C:\\Users\\Affaan Jaweed\\Desktop\\expenset\\templates")
     
    
     app.config.from_object(Config)
