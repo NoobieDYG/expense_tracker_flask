166a4bd833b2799b54e3c2b486199211b0daf426
Affaan Jaweed <73639438+NoobieDYG@users.noreply.github.com>
2024-12-08 22:03:39 +0530

    updated config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost/expense'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost/expense' #enter your address if using mysql
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
